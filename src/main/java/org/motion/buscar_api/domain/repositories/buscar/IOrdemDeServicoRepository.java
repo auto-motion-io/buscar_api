a9a484614c8d00ed1fff9c5c59ab150af4515054
matheus <matheus.slima@sptech.school>
2024-05-17 14:55:28 -0300

    delete repository ods

--- src/main/java/org/motion/buscar_api/domain/repositories/buscar/IOrdemDeServicoRepository.java
+++
@@ -1,7 +0,0 @@
-package org.motion.buscar_api.domain.repositories.buscar;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IOrdemDeServicoRepository extends JpaRepository<OrdemDeServico,Integer> {
-    OrdemDeServico findByToken (String token);
-}

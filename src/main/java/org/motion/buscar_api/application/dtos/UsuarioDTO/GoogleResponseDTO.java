0a586685ccf71735a5c1d92b486ec4acddf0555d
Matheus Lima <matheus.slima@sptech.school>
2024-11-28 20:58:11 -0300

    fix chave to token google response

--- src/main/java/org/motion/buscar_api/application/dtos/UsuarioDTO/GoogleResponseDTO.java
+++ src/main/java/org/motion/buscar_api/application/dtos/UsuarioDTO/GoogleResponseDTO.java
@@ -3,6 +3,7 @@ package org.motion.buscar_api.application.dtos.UsuarioDTO;
 public record GoogleResponseDTO(
         Integer idUsuario,
         String nome,
-        String chave,
-        String fotoUrl) {
+        String token,
+        String fotoUrl
+) {
 }
